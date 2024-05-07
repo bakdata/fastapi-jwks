307e661b3e751f7a1ddd9d022bb78c33883c9843
Alejandro Jaramillo <alejojaramillo96@gmail.com>
2024-05-07 18:34:48 +0200

    Add first version for fastapi-jwks package (#5)

---
+++ tests/injector/test_injector.py
@@ -0,0 +1,18 @@
+from unittest.mock import MagicMock
+
+import pytest
+from pydantic import BaseModel
+
+from fastapi_jwks.injector import JWTTokenInjector
+
+
+class FakeUser(BaseModel):
+    user: str
+
+
+@pytest.mark.asyncio()
+async def test_injector_return_call():
+    user = FakeUser(user="my-fake-user")
+    mock_request = MagicMock(state=MagicMock(payload=user))
+    injector = JWTTokenInjector[FakeUser]()
+    assert (await injector(mock_request)) == user
