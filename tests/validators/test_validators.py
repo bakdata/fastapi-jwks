72ffa87ca76ebffb54eee275ff5d1a3ab5de1ade
Torben Meyer <torben.meyer@bakdata.com>
2025-08-25 09:58:17 +0200

    test: add validation test for RSA key (#145)

--- tests/validators/test_validators.py
+++ tests/validators/test_validators.py
@@ -1,86 +1,97 @@
 import base64
+import dataclasses
 import datetime
 import tempfile
-from collections.abc import Callable
 from datetime import timezone
+from pathlib import Path
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 import jwt
 import pytest
+from jwt.algorithms import RSAAlgorithm
 from pydantic import BaseModel
 
 from fastapi_jwks.models.types import JWKS, JWKSConfig, JWTDecodeConfig
 from fastapi_jwks.validators import JWKSValidator
+
+RESOURCE_PATH = Path(__file__).parent / "resources"
+
+KID = "secret"
+"""KID used across all JWKS"""
 
 
 class FakeToken(BaseModel):
     user: str
 
 
-def jwks_fake_data() -> JWKS:
-    return JWKS.model_validate(
-        {
-            "keys": [
-                {
-                    "kty": "oct",
-                    "use": "sig",
-                    "kid": "sYW9Qh23pPfbD06_F4UY6oAdi2FlNTwBAV6L6YMLY3o",
-                    "k": "b3NFUGVJR09BRW1JMzd6UTdYLUtaT0haci1ZUTZSVzhqaGd0QVhBdThKazZMSWFMclg3TXJsTHJ3YTZXenM3NWI4U1l3em1sQ0VLdXlJeXpVeXNDMmRLeVZ5RkVHSHZ5OWdtNk1PSGRTWjZXWDdWN3VIMHpaZmlkbDZhVV9LYTI0dnF3WHlYaXBKWHV5LWJoMVl4U0w4M0RRVnhmbk43X2NSMHNGbzVoSmFhUnJpT2NYWUt2SEJ2YXQ0dHFRMldJZnNTenJxdTA5alY0RFN4TjdXaTJ5NHJrU1dmVXY4cVV2ZU9OUHVUc3hQQURRb3RKdExsMUtEeGRjUHFIVkZPUTRmODhMZkZJb3ZreXZsNEZiSHM3Q05Uejh2Z0Etdml2cGhRNXJyVGVuUjUxaUd0c0lybC14V29KZXFzQ3lDVXdGdzl2SmxheFhqWXM0TDBsT3dLcGVR",
-                    "alg": "HS256",
-                }
-            ]
-        }
-    )
+@dataclasses.dataclass
+class JwksToken:
+    jwks: JWKS
+    signed_token: str
 
 
-def jwks_fake_data_no_alg() -> JWKS:
-    return JWKS.model_validate(
-        {
-            "keys": [
-                {
-                    "kty": "oct",
-                    "use": "sig",
-                    "kid": "sYW9Qh23pPfbD06_F4UY6oAdi2FlNTwBAV6L6YMLY3o",
-                    "k": "b3NFUGVJR09BRW1JMzd6UTdYLUtaT0haci1ZUTZSVzhqaGd0QVhBdThKazZMSWFMclg3TXJsTHJ3YTZXenM3NWI4U1l3em1sQ0VLdXlJeXpVeXNDMmRLeVZ5RkVHSHZ5OWdtNk1PSGRTWjZXWDdWN3VIMHpaZmlkbDZhVV9LYTI0dnF3WHlYaXBKWHV5LWJoMVl4U0w4M0RRVnhmbk43X2NSMHNGbzVoSmFhUnJpT2NYWUt2SEJ2YXQ0dHFRMldJZnNTenJxdTA5alY0RFN4TjdXaTJ5NHJrU1dmVXY4cVV2ZU9OUHVUc3hQQURRb3RKdExsMUtEeGRjUHFIVkZPUTRmODhMZkZJb3ZreXZsNEZiSHM3Q05Uejh2Z0Etdml2cGhRNXJyVGVuUjUxaUd0c0lybC14V29KZXFzQ3lDVXdGdzl2SmxheFhqWXM0TDBsT3dLcGVR",
-                }
-            ]
-        }
-    )
-
-
-@pytest.fixture()
-def signed_token() -> str:
-    jwk = jwks_fake_data().keys[0]
-    key = jwk.k
-    assert key
-    algo = jwk.alg
-    kid = jwk.kid
-
+def create_signed_jwt(key: str | bytes, alg: str | None = None) -> str:
     claim = {
         "user": "my-fake-user",
         "iat": datetime.datetime.now(timezone.utc).timestamp(),
     }
-    signed_token = jwt.encode(
-        claim, base64.urlsafe_b64decode(key), headers={"kid": kid}, algorithm=algo
+    return jwt.encode(claim, key, headers={"kid": KID}, algorithm=alg)
+
+
+def new_hs256_jwks(key: dict[str, Any]):
+    decoded_key = base64.urlsafe_b64decode(key["k"])
+    return JwksToken(
+        JWKS.model_validate({"keys": [key]}),
+        create_signed_jwt(decoded_key, key.get("alg")),
     )
-    return signed_token
+
+
+def new_rsa_jwks():
+    """Create a new RSA JWKS with the given public key and private key."""
+    algo = RSAAlgorithm(RSAAlgorithm.SHA256)
+    key = algo.prepare_key((RESOURCE_PATH / "public-key.pem").read_text())
+    key = algo.to_jwk(key, as_dict=True) | {"alg": "RS256", "kid": KID}
+    return JwksToken(
+        JWKS.model_validate({"keys": [key]}),
+        create_signed_jwt((RESOURCE_PATH / "private-key.pem").read_bytes(), key["alg"]),
+    )
+
+
+TEST_JWKS = {
+    "HS256": new_hs256_jwks(
+        {
+            "kty": "oct",
+            "use": "sig",
+            "kid": KID,
+            "k": "b3NFUGVJR09BRW1JMzd6UTdYLUtaT0haci1ZUTZSVzhqaGd0QVhBdThKazZMSWFMclg3TXJsTHJ3YTZXenM3NWI4U1l3em1sQ0VLdXlJeXpVeXNDMmRLeVZ5RkVHSHZ5OWdtNk1PSGRTWjZXWDdWN3VIMHpaZmlkbDZhVV9LYTI0dnF3WHlYaXBKWHV5LWJoMVl4U0w4M0RRVnhmbk43X2NSMHNGbzVoSmFhUnJpT2NYWUt2SEJ2YXQ0dHFRMldJZnNTenJxdTA5alY0RFN4TjdXaTJ5NHJrU1dmVXY4cVV2ZU9OUHVUc3hQQURRb3RKdExsMUtEeGRjUHFIVkZPUTRmODhMZkZJb3ZreXZsNEZiSHM3Q05Uejh2Z0Etdml2cGhRNXJyVGVuUjUxaUd0c0lybC14V29KZXFzQ3lDVXdGdzl2SmxheFhqWXM0TDBsT3dLcGVR",
+            "alg": "HS256",
+        }
+    ),
+    "NO_ALG": new_hs256_jwks(
+        {
+            "kty": "oct",
+            "use": "sig",
+            "kid": KID,
+            "k": "b3NFUGVJR09BRW1JMzd6UTdYLUtaT0haci1ZUTZSVzhqaGd0QVhBdThKazZMSWFMclg3TXJsTHJ3YTZXenM3NWI4U1l3em1sQ0VLdXlJeXpVeXNDMmRLeVZ5RkVHSHZ5OWdtNk1PSGRTWjZXWDdWN3VIMHpaZmlkbDZhVV9LYTI0dnF3WHlYaXBKWHV5LWJoMVl4U0w4M0RRVnhmbk43X2NSMHNGbzVoSmFhUnJpT2NYWUt2SEJ2YXQ0dHFRMldJZnNTenJxdTA5alY0RFN4TjdXaTJ5NHJrU1dmVXY4cVV2ZU9OUHVUc3hQQURRb3RKdExsMUtEeGRjUHFIVkZPUTRmODhMZkZJb3ZreXZsNEZiSHM3Q05Uejh2Z0Etdml2cGhRNXJyVGVuUjUxaUd0c0lybC14V29KZXFzQ3lDVXdGdzl2SmxheFhqWXM0TDBsT3dLcGVR",
+        }
+    ),
+    "RSA256": new_rsa_jwks(),
+}
 
 
 @pytest.mark.parametrize(
-    "jwks_data_provider",
-    [
-        jwks_fake_data,
-        jwks_fake_data_no_alg,
-    ],
+    "jwks_token",
+    TEST_JWKS.values(),
 )
 def test_simple_validate(
     monkeypatch: pytest.MonkeyPatch,
-    signed_token: str,
-    jwks_data_provider: Callable[[], JWKS],
+    jwks_token: JwksToken,
 ):
+    signed_token = jwks_token.signed_token
     monkeypatch.setattr(
         "fastapi_jwks.validators.jwks_validator.JWKSValidator.jwks_data",
-        lambda _: jwks_data_provider(),
+        lambda _: jwks_token.jwks,
     )
     validator = JWKSValidator[FakeToken](
         decode_config=JWTDecodeConfig(),
@@ -94,13 +105,13 @@ def test_simple_validate(
 @patch("jwt.decode")
 @patch(
     "fastapi_jwks.validators.jwks_validator.JWKSValidator.jwks_data",
-    return_value=jwks_fake_data(),
+    return_value=TEST_JWKS["HS256"].jwks,
 )
 def test_extra_config(
     data_mock: MagicMock,
     jwt_decode: MagicMock,
-    signed_token: str,
 ):
+    signed_token = TEST_JWKS["HS256"].signed_token
     token = FakeToken(user="my-fake-user")
     jwt_decode.return_value = token
     validator = JWKSValidator[FakeToken](
@@ -113,7 +124,7 @@ def test_extra_config(
         ),
         jwks_config=JWKSConfig(url="https://my-fake-jwks-endpoint/my-endpoint"),
     )
-    jwks_data = jwks_fake_data()
+    jwks_data = TEST_JWKS["HS256"].jwks
     key = jwks_data.keys[0].k
     assert key
     algo = jwks_data.keys[0].alg
@@ -135,9 +146,10 @@ def test_extra_config(
 
 @patch(
     "fastapi_jwks.validators.jwks_validator.JWKSValidator.jwks_data",
-    return_value=jwks_fake_data(),
+    return_value=TEST_JWKS["HS256"].jwks,
 )
-def test_generic_mandatory(data_mock, signed_token):
+def test_generic_mandatory(data_mock):
+    signed_token = TEST_JWKS["HS256"].signed_token
     validator = JWKSValidator(
         decode_config=JWTDecodeConfig(),
         jwks_config=JWKSConfig(url="https://my-fake-jwks-endpoint/my-endpoint"),
@@ -158,7 +170,7 @@ def test_custom_ca_cert():
 
         with patch("httpx.Client") as mock_client:
             mock_response = MagicMock()
-            mock_response.json.return_value = jwks_fake_data()
+            mock_response.json.return_value = TEST_JWKS["HS256"].jwks
             mock_response.raise_for_status.return_value = None
             mock_client.return_value.get.return_value = mock_response
 
@@ -172,7 +184,7 @@ def test_custom_ca_cert():
 
             jwks_data = jwks_verifier.jwks_data()
 
-            assert jwks_data == jwks_fake_data()
+            assert jwks_data == TEST_JWKS["HS256"].jwks
             mock_client.assert_called_with(verify=ca_cert_file.name)
             mock_client.return_value.get.assert_called_with(
                 "https://my-fake-jwks-endpoint/my-endpoint"
